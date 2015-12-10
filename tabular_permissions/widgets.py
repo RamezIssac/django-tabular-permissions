606373d521db349a7ff35261db7a03b6fa475a8a
ramezashraf <ramezashraf@gmail.com>
2015-12-10 14:39:50 +0200

    Fix django 1.8/1.9 compatibility

--- tabular_permissions/widgets.py
+++ tabular_permissions/widgets.py
@@ -1,30 +1,31 @@
 from __future__ import unicode_literals
 import pdb
+from django import __version__ as django_version
+from django.apps import apps
 from django.contrib.auth.models import Permission
+from django.contrib.admin.templatetags.admin_static import static
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.template.loader import get_template
 from django.utils.encoding import force_text
 from django.utils.html import escapejs
 from django.utils.safestring import mark_safe
-from django.apps import apps
 from . import app_settings
 from .helpers import get_perm_name
 
 
 class TabularPermissionsWidget(FilteredSelectMultiple):
-
     class Media:
         js = ('tabular_permissions/tabular_permissions.js',)
 
     def __init__(self, verbose_name, is_stacked, input_name='user_permissions', attrs=None, choices=()):
         super(TabularPermissionsWidget, self).__init__(verbose_name, is_stacked, attrs, choices)
         self.exclude_perms = []
-        self.input_name = input_name  # in case of UserAdmin, it's 'user_permissions' , GroupAdmin it's 'permissions'
+        self.input_name = input_name  # in case of UserAdmin, it's 'user_permissions', GroupAdmin it's 'permissions'
         self.hide_original = True
 
     def render(self, name, value, attrs=None, choices=()):
 
-        apps_available = [] # main container to send to template
+        apps_available = []  # main container to send to template
         user_permissions = Permission.objects.filter(id__in=value or []).values_list('id', flat=True)
         all_perms = Permission.objects.all().values('id', 'codename').order_by('codename')
         excluded_perms = []
@@ -80,12 +81,16 @@ class TabularPermissionsWidget(FilteredSelectMultiple):
                 apps_available.append(app_dict)
 
         request_context = {'apps_available': apps_available, 'user_permissions': user_permissions,
-                           'codename_id_map': codename_id_map, 'input_name':self.input_name}
+                           'codename_id_map': codename_id_map, 'input_name': self.input_name}
         body = get_template(app_settings.TABULAR_PERMISSIONS_TEMPLATE).render(request_context).encode("utf-8")
         self.exclude_perms = excluded_perms
+        if reminder_perms:
+            self.hide_original = False
 
         # Get "original" FilteredSelectMultiple, and hide it if necessary.
         # Next block is a "copy" of FilteredSelectMultiple render(), except the if reminder_perms: check.
+        # Due to change in how SelectFilter take its arguments and the dropping of static('admin/') in django1.9
+        # there a check on django version
 
         if attrs is None:
             attrs = {}
@@ -94,16 +99,18 @@ class TabularPermissionsWidget(FilteredSelectMultiple):
             attrs['class'] += 'stacked'
 
         output = [super(FilteredSelectMultiple, self).render(name, value, attrs, choices)]
-        # pdb.set_trace()
         if reminder_perms:
             output.append('<script type="text/javascript">addEvent(window, "load", function(e) {')
-            # TODO: "id_" is hard-coded here. This should instead use the correct
-            # API to determine the ID dynamically.
-            output.append('SelectFilter.init("id_%s", "%s", %s); });</script>\n'
-                % (name, escapejs(self.verbose_name), int(self.is_stacked)))
-        initial = mark_safe(''.join(output))
 
-        response = u'%s <hr/> %s' % (force_text(body), force_text(initial))
+            if django_version == '1.8':
+                output.append('SelectFilter.init("id_%s", "%s", %s, "%s"); });</script>\n'
+                              % (name, self.verbose_name.replace('"', '\\"'), int(self.is_stacked), static('admin/')))
+            else:  # 1.9
+                output.append('SelectFilter.init("id_%s", "%s", %s); });</script>\n'
+                              % (name, escapejs(self.verbose_name), int(self.is_stacked)))
+
+        initial = mark_safe(''.join(output))
+        response = u'%s <hr/> %s' % (body, initial)
         return mark_safe(response)
 
     def render_option(self, selected_choices, option_value, option_label):
