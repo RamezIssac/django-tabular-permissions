08f7ea3c2103bc52270d2f1799b801f4e2ad1994
RamezIssac <ramez@rasystems.com>
2019-12-19 10:10:33 +0200

    Update travis configurations and tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.static',
